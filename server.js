534bea8f8adb9c91aed77ff9eff9a2d94fc8e60c
Abraham, Jameelah, Victor <emmanuelabraham525@gmail.com, meelanyahmed@yahoo.com, victorjoel0061@gmail.com>
2024-11-29 12:22:06 +0100

    Added comments in the index.ejs and server.js files

--- server.js
+++ server.js
@@ -1,27 +1,34 @@
-import express from 'express';
-import path from 'path';
-import {fileURLToPath} from 'url';
+//import necessary modules
+import express from 'express'; // Import Express.js framework
+import path from 'path'; // // Import the 'path' module for handling file and directory paths
+import {fileURLToPath} from 'url';  // Import 'fileURLToPath' to get the current file's path when using ES modules
 
-const app = express();
-const PORT = 5000;
 
+const app = express(); 
+const PORT = 5000;  // Define the port the server will listen on
+
+// ES modules does not provide __dirname by default, so, get the current file's path and directory
 const __filename = fileURLToPath(import.meta.url);
 const __dirname =  path.dirname(__filename);
  
 
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+// Set up the view engine for rendering dynamic templates
+app.set("view engine", "ejs"); // Use Ejs as the templating engine
+app.set("views", path.join(__dirname, "views")); // Define the folder where Ejs templates are located
 
-app.use(express.static(path.join(__dirname, "public")));
-app.use("/css", express.static("dist"))
+//Serve static files
+app.use(express.static(path.join(__dirname, "public"))); // Serve static files from the 'public' folder
 
 
+
+// Define a Route for the countdown endpoint
 app.get("/countdown", (req, res) => { 
    res.render('index', {
-       pageTitle : "countdown"
+       pageTitle : "countdown" // Pass the 'pageTitle' variable to the Ejs template
    })
 });   
 
+//Start the server and listen on the defined port
 app.listen(PORT, () => {
     console.log(`server is running on http://localhost:${PORT}`);
 })
